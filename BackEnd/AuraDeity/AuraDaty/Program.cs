9054d68c16dbddd3cf2cd5018cfe64637561bc19
Alex Smarandache <alex.smarandache@yahoo.com>
2022-08-02 23:58:50 +0300

    Add Weather API

---
+++ BackEnd/AuraDeity/AuraDaty/Program.cs
@@ -0,0 +1,49 @@
+using DataAccess;
+using Interfaces.Commands;
+using Interfaces.Queries;
+using Interfaces.Queries.Services;
+using Logic;
+using Logic.Commands;
+using Logic.Queries;
+using Logic.Queries.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// Add services to the container.
+builder.Services.AddScoped<IAuthenticationCommand, AuthenticationCommand>();
+builder.Services.AddScoped<IAuthenticationQuery, AuthenticationQuery>();
+builder.Services.AddScoped<IJwtQueryService, JwtQueryService>();
+builder.Services.AddScoped<IWeatherAPI, WeatherAPI>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuraDeityContext>(dbOptions =>
+{
+    dbOptions.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseCors(
+    options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
